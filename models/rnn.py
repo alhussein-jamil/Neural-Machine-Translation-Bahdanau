2f38ef0a4b74a6b6dd6984fd404c6c0c4baa684b
alhussein-jamil <alhussein.jamil@polytechnique.edu>
2023-11-13 22:15:57 +0100

    added database download

--- models/rnn.py
+++ models/rnn.py
@@ -9,3 +9,13 @@ class RNN(nn.Module):
         self.num_layers = num_layers
         self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        # Set initial hidden and cell states 
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+        # Forward propagate LSTM
+        out, _ = self.rnn(x, h0)
+        # Decode the hidden state of the last time step
+        out = self.fc(out[:, -1, :])
+        return out
+    
