5f4a92b6e22cdc27e4e926a08e63cd6567aa2d00
alhussein-jamil <alhussein.jamil@polytechnique.edu>
2023-11-14 17:01:34 +0100

    RNN model update

--- src/models/rnn.py
+++ src/models/rnn.py
@@ -2,20 +2,24 @@ import torch.nn as nn
 import torch
 
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, device): 
-        super(RNN, self).__init__()
-        self.device = device
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, 1)
+    def __init__(self, input_size, hidden_size, num_layers, device, activation="tanh", dropout=0):
 
+        super(RNN, self).__init__()
+        
+        self.device = device
+        
+        self.hidden_size = hidden_size
+        
+        self.num_layers = num_layers
+        
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, nonlinearity=activation, batch_first=True, dropout=dropout).to(self.device)  
+        
     def forward(self, x):
-        # Set initial hidden and cell states 
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-        # Forward propagate LSTM
-        out, _ = self.rnn(x, h0)
-        # Decode the hidden state of the last time step
-        out = self.fc(out[:, -1, :])
-        return out
-    
+            
+            # Initialize hidden state
+            h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
+            
+            # Forward propagate RNN
+            out, _ = self.rnn(x, h0)
+            
+            return out
