47173519605426472d85fed2c19410b82b162054
IKRAMMES <ikram.mesbah@etu.sorbonne-universite.fr>
2023-11-15 11:29:38 +0100

    Description du commit

--- src/models/EncoDeco.py
+++ src/models/EncoDeco.py
@@ -93,3 +93,45 @@ class Attention(nn.Module):
         energy = torch.tanh(self.attn(torch.cat((hidden, encoder_outputs), dim = 2))) 
         attention = self.v(energy).squeeze(2)
         return F.softmax(attention, dim=1)
+    
+#seq2seq   
+class Seq2Seq(nn.Module):
+    def __init__(self, encoder, decoder, device):
+        super().__init__()
+        
+        self.encoder = encoder
+        self.decoder = decoder
+        self.device = device
+        
+        assert encoder.hid_dim == decoder.hid_dim, \
+            "Hidden dimensions of encoder and decoder must be equal!"
+        assert encoder.n_layers == decoder.n_layers, \
+            "Encoder and decoder must have equal number of layers!"
+        
+    def forward(self, src, trg, teacher_forcing_ratio = 0.5):
+    
+        batch_size = trg.shape[1]
+        trg_len = trg.shape[0]
+        trg_vocab_size = self.decoder.output_dim
+        
+        outputs = torch.zeros(trg_len, batch_size, trg_vocab_size).to(self.device)# tenseur pour stocker les sorties du décodeur
+        
+        hidden, cell = self.encoder(src) # le dernier état caché de l'encodeur est utilisé comme état caché initial du décodeur
+
+        
+        input = trg[0,:]
+        
+        for t in range(1, trg_len):
+            
+
+            output, hidden, cell = self.decoder(input, hidden, cell)
+            
+            outputs[t] = output
+            
+            teacher_force = random.random() < teacher_forcing_ratio
+        
+            top1 = output.argmax(1) 
+            
+            input = trg[t] if teacher_force else top1
+        
+        return outputs
