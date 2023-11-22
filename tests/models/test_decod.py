94c66e197f51b5b25f5096537b7f2c34bc9944d7
MohamedLokmane <mohamed_islam.lokmane@etu.sorbonne-universite.fr>
2023-11-22 13:02:21 +0100

    Ajout_test_decodeur

--- tests/models/test_decod.py
+++ tests/models/test_decod.py
@@ -23,6 +23,28 @@ hidden_state = encoder(input_data)
 # Afficher la taille du tenseur résultant
 print("Hidden state size:", hidden_state.size())
 
+######### DECODEUR
+output_size = 20
+emb_size = 10
+hidden_size = 10
+num_layers = 2
+dropout_proba = 0.1
+
+# Créer une instance du décodeur
+decoder = Decoder(output_size, emb_size, hidden_size, num_layers, device, dropout_proba)
+
+# Définir des données factices pour le test
+input_data = torch.tensor([1])  
+hidden_state = torch.zeros(num_layers, 1, hidden_size).to(device) 
+context_data = torch.zeros(1, 1, emb_size).to(device)  
+
+# Passer les données à travers le décodeur
+output, new_hidden_state = decoder(input_data, hidden_state, context_data)
+
+# Afficher les résultats
+print("Output shape:", output.shape)
+print("New hidden state shape:", new_hidden_state.shape)
+
 
 
 # Paramètres du modèle
