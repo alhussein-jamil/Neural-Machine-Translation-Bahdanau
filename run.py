Fix alignment vector computation in Decoder

run.py
@@ -41,12 +41,23 @@ if __name__ == "__main__":
     ) = load_data(
         train_len=args.train_len, val_len=args.val_len, n=30000, m=30000, Tx=30, Ty=30
     )
+    device = "cpu" if not torch.cuda.is_available() else "cuda"
 
+
+    config_rnn_decoder = dict(
+            input_size=args.enc_out_size * 2,
+            hidden_size=10,
+            num_layers=1,
+            device=device,
+            dropout=0,
+            type="GRU",
+            bidirectional=False,
+        )
     alignment_cfg = dict(
-            input_size=args.enc_out_size + args.Ty,
+            input_size=args.enc_out_size * 2 + config_rnn_decoder["hidden_size"],
             hidden_sizes=[10, 10],
             output_size=args.Ty,
-            device= "cpu" if not torch.cuda.is_available() else "cuda",
+            device= device,
             activation=torch.nn.ReLU(),
             last_layer_activation=torch.nn.Sigmoid(),
             dropout=0.2,
@@ -54,4 +65,3 @@ if __name__ == "__main__":
     )
 =======
     )
->>>>>>> ea636e9 (Refactor code and update dependencies)

src/models/decoder.py
@@ -47,25 +47,26 @@ class Decoder(nn.Module):
         Returns:
             torch.Tensor: Tensor containing the predicted indices of the output tokens.
         """
-        # h is the output of the encoder and has sizes (batch_size, Tx, hidden_size),
-        # this function outputs a tensor of size (batch_size, Ty) containing the predicted indices of the output tokens
-        # first, we need to find the context vector c
-        # we do this by finding the alignment vector a
+        # Initialize output tensor
         output = torch.zeros(h.size(0), h.size(1), self.rnn.hidden_size).to(h.device)
 
+        # Initialize context vector
         s = torch.zeros(
-            self.rnn.num_layers * 1 if not self.rnn.rnn.bidirectional else 2,
+            self.rnn.num_layers * (1 if not self.rnn.rnn.bidirectional else 2),
             h.size(0),
             self.rnn.hidden_size,
         ).to(h.device)
 
         for i in range(h.size(1)):
+            # Compute alignment vector
             a = self.alignment(s.squeeze(0), h[:, i, :])
             e = F.softmax(a, dim=1)
-            c = (h.swapaxes(1, 2) @ e.unsqueeze(2)).squeeze(
-                2
-            )  # c is of shape (batch_size, hidden_size)
+
+            # Compute context vector
+            c = torch.bmm(h.transpose(1, 2), e.unsqueeze(2)).squeeze(2)
+
+            # Compute output and update context vector
             y, s = self.rnn(c.unsqueeze(1), s)
             output[:, i, :] = y.squeeze(1)
-  
+
         return F.softmax(output, dim=2)