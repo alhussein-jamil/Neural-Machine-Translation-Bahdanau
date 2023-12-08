60981dfd7fdf64ff19e65fce5dbe3c321549a36e
NadiaABDELFETTAH <nadia.abdelfettah@etu.sorbonne-universite.fr>
2023-12-08 02:46:23 +0100

    add improuvement on test translation model

--- tests/models/test_translation_model.py
+++ tests/models/test_translation_model.py
@@ -1,73 +1,96 @@
 import unittest
 import torch
-from torch import nn
-from models.translation_models import AlignAndTranslate
+from torch import nn, optim
+from models.translation_model import AlignAndTranslate
+from models.encoder import Encoder
+from models.decoder import Decoder 
+from models.rnn import RNN
 
+
+# HAVE TO VERIFY DIMENSIONS OF WEIGHTS OF FCNN 
 class TestAlignAndTranslate(unittest.TestCase):
+
     def setUp(self):
-        # Initialisation pour chaque test
-        self.encoder_params = {"input_size": 100, "hidden_size": 50}
-        self.decoder_params = {"output_size": 10}
-        self.training_config = {
-            "criterion": torch.nn.CrossEntropyLoss(),
-            "optimizer": torch.optim.Adam(),
-            "device": "cpu",
-            "epochs": 2,
-            "batch_size": 32,
-            "print_every": 100,
-            "save_every": 1000,
-            "checkpoint": "checkpoint.pth",
-            "best_model": "best_model.pth",
-            "output_vocab_size": 100,
+        # Define parameters for testing
+        encoder_params = {
+            "rnn_hidden_size": 5,
+            "rnn_num_layers": 1,
+            "rnn_device": "cpu",
+            "vocab_size": 5,
+            "rnn_type": "GRU"
         }
 
-        self.model = AlignAndTranslate(encoder=self.encoder_params, decoder=self.decoder_params, training=self.training_config)
+        decoder_params = {
+            "alignment": {
+                "input_size": encoder_params["rnn_hidden_size"] * 2,
+                "hidden_sizes": [10],
+                "output_size": 5,
+                "device": "cpu"
+            },
+            "rnn": {
+                "input_size": encoder_params["rnn_hidden_size"] * 2,
+                "hidden_size": 10,
+                "num_layers": 1,
+                "device": "cpu",
+                "dropout": 0,
+                "type": "GRU",
+                "bidirectional": False,
+            }
+        }
 
-        # Exemple de données factices pour les tests
-        self.example_input_data = torch.randn((32, 100))  
-        self.example_target_data = torch.randn((32, 100))  
+        optimizer_params = {
+            "lr": 0.001,  # Taux d'apprentissage  
+            "momentum": 0.9,  # Moment 
+            "weight_decay": 0,  # Terme de dégradation du poids 
+        }
 
-    def test_forward_pass(self):
-        # Vérifier si la passe avant (forward pass) fonctionne sans erreurs
-        output_tensor = self.model(self.example_input_data)
-        assert output_tensor.shape == torch.Size([32, 10])  # Adapter la taille en fonction de votre modèle
+        criterion = torch.nn.CrossEntropyLoss()
 
-    def test_train_step(self):
-        # Vérifier si une étape d'entraînement fonctionne sans erreurs
-        loss = self.model.train_step(self.example_input_data, self.example_target_data)
-        assert loss >= 0
-
-    def test_train(self):
-        # Vérifier si la fonction d'entraînement sur plusieurs époques fonctionne sans erreurs
-        train_dataset = torch.utils.data.TensorDataset(self.example_input_data, self.example_target_data)
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True)
-
-        val_dataset = torch.utils.data.TensorDataset(self.example_input_data, self.example_target_data)
-        val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=32, shuffle=False)
-
-        self.model.train(train_loader, val_loader)
-
-
-    def test_save_model(self):
-        # Vérifier si la fonction de sauvegarde du modèle fonctionne sans erreurs
-        self.model.save_model("test_checkpoint.pth")
-        assert torch.isfile("test_checkpoint.pth")  
+        # Create an instance of the AlignAndTranslate model
+        self.model = AlignAndTranslate(
+            encoder_params=encoder_params,
+            decoder_params=decoder_params,
+            optimizer_params=optimizer_params,
+            criterion=criterion
+        )
 
     def test_model_initialization(self):
-        # Vérifier si l'initialisation du modèle est correcte
+        # Check if the model is an instance of nn.Module
         self.assertIsInstance(self.model, nn.Module)
-        self.assertEqual(self.model.input_size, self.encoder_params["input_size"])
-        self.assertEqual(self.model.hidden_size, self.encoder_params["hidden_size"])
-        self.assertEqual(self.model.output_size, self.decoder_params["output_size"])
-        self.assertIsInstance(self.model.optimizer, torch.optim.Adam)
-        self.assertEqual(self.model.device, self.training_config["device"])
-        self.assertEqual(self.model.epochs, self.training_config["epochs"])
-        self.assertEqual(self.model.batch_size, self.training_config["batch_size"])
-        self.assertEqual(self.model.print_every, self.training_config["print_every"])
-        self.assertEqual(self.model.save_every, self.training_config["save_every"])
-        self.assertEqual(self.model.checkpoint, self.training_config["checkpoint"])
-        self.assertEqual(self.model.best_model, self.training_config["best_model"])
-        self.assertEqual(self.model.output_vocab_size, self.training_config["output_vocab_size"])
+
+        # Check if the encoder and decoder are instances of their respective classes
+        self.assertIsInstance(self.model.encoder, Encoder)
+        self.assertIsInstance(self.model.decoder, Decoder)
+
+        # Check if the optimizer and criterion are initialized correctly
+        self.assertIsInstance(self.model.optimizer, optim.SGD)  
+        self.assertIsInstance(self.model.criterion, nn.CrossEntropyLoss)
+
+    def test_forward_pass(self):
+        # Check if the forward pass runs without errors
+        input_data = torch.randint(0, self.model.encoder.vocab_size, (32, 10)).to("cpu")
+        output_tensor = self.model(input_data)
+        # Adjust the expected shape based on your implementation
+        expected_shape = torch.Size([32, 10, 5])  # Adapt based on your output size
+        self.assertEqual(output_tensor.shape, expected_shape)
+
+    def test_training_step(self):
+        # Check if the training step runs without errors
+        batch = {
+            "english": {"idx": torch.randint(0, self.model.encoder.vocab_size, (32, 10), dtype=torch.long)},
+            "french": {"idx": torch.randint(0, 5, (32, 10), dtype=torch.long)}
+        }
+        loss = self.model.train_step(batch["english"]["idx"], batch["french"]["idx"])
+        self.assertIsNotNone(loss)
+
+    def test_predict(self):
+        # Check if the prediction runs without errors
+        batch = {
+            "english": {"idx": torch.randn(32, 10, self.model.encoder.vocab_size)},
+            "french": {"idx": torch.randint(0, 5, (32, 10), dtype=torch.long)}
+        }
+        predictions = self.model.predict(batch["english"]["idx"])
+        self.assertIsNotNone(predictions)
 
 if __name__ == '__main__':
     unittest.main()
