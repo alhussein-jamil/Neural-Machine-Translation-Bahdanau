6dcd97df0fa61a87eada026c9fd7a0279882d998
MohamedLokmane <150231692+MohamedLokmane@users.noreply.github.com>
2023-12-09 18:37:33 +0100

    score bleu (manuel+nltk), métriques NLL et CrossEntropy avec fichiers tests (#6)
    * modifs_pour_merge
    
    * AJOUT_CLASSE_BLEU_SCORE
    
    * BLEUscore_classe(manuel+nltk)
    
    * NLLLoss_classe_metric
    
    * AJOUT_CLASSE_BLEU_SCORE_SANS_LE_MAIN
    
    * BLEU_SCORE_NEW
    
    * BELU_SCORE_NLTK_NEW
    
    * BLEU_SCORE_NLTK_NEW
    
    * NLLLoss_NEW
    
    * test_bleu_score
    
    * test_bleu_score_nltk
    
    * test_NLLLoss
    
    * BLEU_SCORE (MANUAL+NLTK) & NLLLOSS METRICS ADDED WITH TEST_NEW
    
    * Add nltk and transformers to requirements.txt
    
    * Ajout métriques (Bleu score (+nltk), NLL+CrossEntropy Losses Avec fichiers tests
    
    * Score Bleu modified
    
    * Bleuscore+NLLLoss
    
    * Bleu_score+nltk
    
    * Bleu score+nltk
    
    * Add BLEU score calculation classes and losses module
    
    * bleu_score+NLLLoss
    
    * bug fixes
    
    ---------
    
    Co-authored-by: alhussein-jamil <alhussein.jamil@polytechnique.edu>

---
+++ tests/metrics/test_Loss.py
@@ -0,0 +1,57 @@
+import unittest
+import torch
+from torch import nn
+from metrics.losses import Loss
+
+class TestLoss(unittest.TestCase):
+    def setUp(self):
+        # Créer des exemples de tenseurs pour les tests
+        self.x = torch.randn(2, 5, 10)  # Exemple de tenseur x
+        self.y = torch.randint(0, 10, (2, 5))  # Exemple de tenseur y
+
+        # Créer une instance de la classe Loss pour les tests
+        self.loss_instance = Loss(loss_fn=nn.CrossEntropyLoss())
+
+    def test_initialization(self):
+        # Vérifier si l'attribut est correctement initialisé
+        assert(isinstance(self.loss_instance.loss_fn, nn.CrossEntropyLoss))
+
+    def test_forward_pass(self):
+        # Exécuter le passage avant avec la méthode de la classe
+        result = self.loss_instance(self.x, self.y)
+
+        # Ajouter des assertions en fonction des résultats attendus
+        self.assertIsInstance(result, torch.Tensor)
+        self.assertTrue(not(result.requires_grad))
+        self.assertFalse(torch.isnan(result).any())
+
+# Créer un test runner et exécuter les tests
+runner = unittest.TextTestRunner()
+result = runner.run(unittest.makeSuite(TestLoss))
+
+
+class TestNLLLoss(unittest.TestCase):
+    def setUp(self):
+        # Créer des exemples de tenseurs pour les tests
+        self.x = torch.rand(2, 5, 10)  # Exemple de tenseur x
+        self.y = torch.randint(0, 10, (2, 5))  # Exemple de tenseur y
+        
+        # Créer une instance de la classe Loss pour les tests
+        self.loss_instance = Loss(loss_fn=nn.NLLLoss())
+
+    def test_initialization(self):
+        # Vérifier si l'attribut est correctement initialisé
+        assert(isinstance(self.loss_instance.loss_fn, nn.NLLLoss))
+
+    def test_forward_pass(self):
+        # Exécuter le passage avant avec la méthode de la classe
+        result = self.loss_instance(self.x, self.y)
+
+        # Ajouter des assertions en fonction des résultats attendus
+        self.assertIsInstance(result, torch.Tensor)
+        self.assertTrue(not(result.requires_grad))
+        self.assertFalse(torch.isnan(result).any())
+
+# Créer un test runner et exécuter les tests
+runner = unittest.TextTestRunner()
+result = runner.run(unittest.makeSuite(TestNLLLoss))
